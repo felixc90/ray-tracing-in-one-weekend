d4e46ea30ddbf3b3b76828a3d9d4e61f6a2ed24e
felixcao <felixcao37@gmail.com>
2025-10-30 13:42:50 +1100

    added fuzz to metal material

--- src/material/metal.rs
+++ src/material/metal.rs
@@ -6,11 +6,15 @@ use crate::{
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Metal { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Metal {
+            albedo,
+            fuzz: f64::min(fuzz, 1.0),
+        }
     }
 }
 
@@ -22,8 +26,9 @@ impl Material for Metal {
         attenuation: &mut Color,
         scattered: &mut Ray,
     ) -> bool {
-        let scatter_direction = Vec3::reflect(&r_in.direction(), &rec.normal);
-        *scattered = Ray::new(rec.p, scatter_direction);
+        let mut reflected = Vec3::reflect(&r_in.direction(), &rec.normal);
+        reflected = reflected + (self.fuzz * Vec3::random_unit_vector());
+        *scattered = Ray::new(rec.p, reflected);
         *attenuation = self.albedo;
         true
     }
